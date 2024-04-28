35b36b8a68da0051b34e1eea9f4655c63ab51c60
Riodejanerio <120099647+Riodejanerio@users.noreply.github.com>
2024-04-28 19:37:02 +0600

    ration.py

--- ration.py
+++ ration.py
@@ -18,7 +18,7 @@ def calculate_ration(bw, lwg):
     elif bw > 300 and lwg > 0.9:
         dmi = bw * .040
     elif bw < 160 and lwg > .5:
-        dmi = bw * .05
+        dmi = bw * .035
     else:
         dmi = bw * 0.015
     

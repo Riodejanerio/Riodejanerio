26df8c28076d2dc0b384b466c6b93ace8c64a079
Riodejanerio <120099647+Riodejanerio@users.noreply.github.com>
2024-05-04 00:21:04 +0600

    AHcgpa.py

--- AHcgpa.py
+++ AHcgpa.py
@@ -1,9 +1,12 @@
+import streamlit as st
+
 def grade():
-    present_cgpa = float(input("Enter your present CGPA: "))
-    target_cgpa = float(input("Enter your targeted CGPA: "))
+    present_cgpa = st.number_input("Enter your present CGPA: ")
+    target_cgpa = st.number_input("Enter your targeted CGPA: ")
     if target_cgpa > 4:
+        st.write("Targeted CGPA cannot be greater than 4.")
         return
-    Number_of_semester_passed = float(input("Number of semester passed to obtain present cgpa: "))
+    Number_of_semester_passed = st.number_input("Number of semester passed to obtain present CGPA: ")
 
     if Number_of_semester_passed == 1:
       total_earned_credit = 20
@@ -22,7 +25,6 @@ def grade():
     if Number_of_semester_passed == 8:
       total_earned_credit = 180
 
-    #total_earned_credit = float(input("Enter your total earned credit till now: "))
     total_credit = 192
     
     target_grade = target_cgpa * total_credit
@@ -31,7 +33,8 @@ def grade():
     required_cgpa = required_grade / (total_credit - total_earned_credit)
 
     if required_cgpa > 4:
-        print("You are not able to gain the targeted CGPA. Better luck next time.")
+        st.write("You are not able to gain the targeted CGPA. Better luck next time.")
     else:
-        print(f"You have to obtain {required_cgpa:.3f} CGPA per rest of the semester")
+        st.write(f"You have to obtain {required_cgpa:.3f} CGPA per rest of the semester")
+
 grade()

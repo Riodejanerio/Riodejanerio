ad1c01e945e78b117ef6c8dd137080d061c38a3e
Riodejanerio <120099647+Riodejanerio@users.noreply.github.com>
2024-05-04 00:53:57 +0600

    Update AHcgpa.py

--- AHcgpa.py
+++ AHcgpa.py
@@ -1,8 +1,8 @@
 import streamlit as st
-total_credit = 192
+
 def grade():
-    present_cgpa = st.number_input("Enter your present CGPA:")
-    target_cgpa = st.number_input("Enter your targeted CGPA:")
+    present_cgpa = st.number_input("Enter your present CGPA:", value=None)
+    target_cgpa = st.number_input("Enter your targeted CGPA:", value=None)
     if target_cgpa is not None and target_cgpa > 4:
         st.write("Targeted CGPA cannot be greater than 4.")
         return
@@ -25,25 +25,16 @@ def grade():
     if Number_of_semester_passed == 8:
       total_earned_credit = 180
 
-    
-    
+    total_credit = 192
     
     target_grade = target_cgpa * total_credit
     present_grade = present_cgpa * total_earned_credit
     required_grade = target_grade - present_grade
     required_cgpa = required_grade / (total_credit - total_earned_credit)
-    
+
     if required_cgpa > 4:
         st.write("You are not able to gain the targeted CGPA. Better luck next time.")
     else:
         st.write(f"You have to obtain {required_cgpa:.3f} CGPA per rest of the semester")
 
-def main():
-    st.title("Required CGPA Calculator for Animal Husbandry Faculty")
-    grade()
-
-    st.text("Developed by 58th Batch of BAU")
-
-
-if __name__ == "__main__":
-    main()
+grade()
